2aab6a65933170a7afe6eb44a869297df631a334
Stefano Varesi <stefano.varesi@gmail.com>
2018-04-25 20:45:19 +0200

    added dependency injection and configuration support

--- subproject/backend/src/main/kotlin/io/github/xstefanox/demo/mf/backend/BackendService.kt
+++ subproject/backend/src/main/kotlin/io/github/xstefanox/demo/mf/backend/BackendService.kt
@@ -2,6 +2,13 @@ package io.github.xstefanox.demo.mf.backend
 
 import mu.KLogging
 import org.apache.camel.main.Main
+import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.singleton
+
+val BACKEND_MODULE = Kodein.Module {
+    bind<BackendService>() with singleton { BackendService() }
+}
 
 class BackendService {
 
--- subproject/rest/src/main/kotlin/io/github/xstefanox/demo/mf/rest/RestService.kt
+++ subproject/rest/src/main/kotlin/io/github/xstefanox/demo/mf/rest/RestService.kt
@@ -3,18 +3,29 @@ package io.github.xstefanox.demo.mf.rest
 import io.undertow.Undertow
 import io.undertow.util.Headers.CONTENT_TYPE
 import mu.KLogging
+import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
 
-class RestService {
+val REST_MODULE = Kodein.Module {
+    bind<RestConfiguration>() with singleton { RestConfiguration(instance()) }
+    bind<RestService>() with singleton { RestService(instance()) }
+}
 
-    private val undertow = Undertow.builder()
-            .addHttpListener(8080, "localhost")
-            .setHandler({ exchange ->
-                exchange.responseHeaders.put(CONTENT_TYPE, "text/plain")
-                exchange.responseSender.send("Hello World")
-            }).build()
+class RestService(restConfiguration: RestConfiguration) {
+
+    private val undertow: Undertow
 
     init {
         System.setProperty("org.jboss.logging.provider", "slf4j")
+
+        undertow = Undertow.builder()
+                .addHttpListener(restConfiguration.port, restConfiguration.host)
+                .setHandler({ exchange ->
+                    exchange.responseHeaders.put(CONTENT_TYPE, "text/plain")
+                    exchange.responseSender.send("Hello World")
+                }).build()
     }
 
     operator fun invoke() {
