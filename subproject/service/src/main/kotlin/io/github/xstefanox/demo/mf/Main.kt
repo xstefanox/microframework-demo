222722f759f54b1912c9f48cd3a146f5a8d1e9bd
Stefano Varesi <stefano.varesi@gmail.com>
2018-05-09 22:21:52 +0200

    better dependency injection and dependency update

--- subproject/service/src/main/kotlin/io/github/xstefanox/demo/mf/Main.kt
+++ subproject/service/src/main/kotlin/io/github/xstefanox/demo/mf/Main.kt
@@ -4,7 +4,6 @@ package io.github.xstefanox.demo.mf
 
 import org.kodein.di.Kodein
 import org.kodein.di.generic.instance
-import org.kodein.di.newInstance
 
 fun main(args: Array<String>) {
 
@@ -12,7 +11,7 @@ fun main(args: Array<String>) {
         import(SERVICE_MODULE)
     }
 
-    val service by kodein.newInstance { Service(instance(), instance()) }
+    val service by kodein.instance<Service>()
 
     service()
 }
--- subproject/service/src/main/kotlin/io/github/xstefanox/demo/mf/Service.kt
+++ subproject/service/src/main/kotlin/io/github/xstefanox/demo/mf/Service.kt
@@ -6,11 +6,15 @@ import io.github.xstefanox.demo.mf.core.CORE_MODULE
 import io.github.xstefanox.demo.mf.rest.REST_MODULE
 import io.github.xstefanox.demo.mf.rest.RestService
 import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
 
 val SERVICE_MODULE = Kodein.Module {
     import(CORE_MODULE)
     import(REST_MODULE)
     import(BACKEND_MODULE)
+    bind<Service>() with singleton { Service(instance(), instance()) }
 }
 
 class Service(
