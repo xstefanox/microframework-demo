a8a5f1ff9dae321e407e00e6efc6727eb68f5568
Stefano Varesi <stefano.varesi@gmail.com>
2018-05-13 12:09:20 +0200

    fixed missing kodein import in rest tests

--- subproject/rest/src/test/kotlin/io/github/xstefanox/demo/mf/rest/TestKodein.kt
+++ subproject/rest/src/test/kotlin/io/github/xstefanox/demo/mf/rest/TestKodein.kt
@@ -2,6 +2,7 @@
 
 package io.github.xstefanox.demo.mf.rest
 
+import io.github.xstefanox.demo.mf.core.CORE_MODULE
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
@@ -12,6 +13,7 @@ val TEST_SERVICE_MODULE = Kodein.Module(allowSilentOverride = true) {
 }
 
 val TEST_KODEIN = Kodein {
+    import(CORE_MODULE)
     import(REST_MODULE)
     import(TEST_SERVICE_MODULE, allowOverride = true)
 }
