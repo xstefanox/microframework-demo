9cd987a1a8c62a03f7c9f2017c8afe16c9c5cb07
Stefano Varesi <stefano.varesi@gmail.com>
2018-05-13 08:30:16 +0200

    actual handler is not needed to be an httphandler

--- subproject/rest/src/main/kotlin/io/github/xstefanox/demo/mf/rest/handler/HelloWorldHandler.kt
+++ subproject/rest/src/main/kotlin/io/github/xstefanox/demo/mf/rest/handler/HelloWorldHandler.kt
@@ -1,10 +1,9 @@
 package io.github.xstefanox.demo.mf.rest.handler
 
 import io.undertow.server.HttpServerExchange
-import io.undertow.server.RoutingHandler
 import io.undertow.util.Headers.CONTENT_TYPE
 
-class HelloWorldHandler: RoutingHandler() {
+class HelloWorldHandler {
 
     fun get(exchange: HttpServerExchange) {
         exchange.responseHeaders.put(CONTENT_TYPE, "text/plain")
