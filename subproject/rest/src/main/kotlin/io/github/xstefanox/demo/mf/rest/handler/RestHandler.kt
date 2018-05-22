524cc5fd682d0e32cdfad1b8c845169451388f58
Stefano Varesi <stefano.varesi@gmail.com>
2018-05-22 15:29:13 +0200

    added an example of request reading in non-blocking way

--- subproject/rest/src/main/kotlin/io/github/xstefanox/demo/mf/rest/handler/RestHandler.kt
+++ subproject/rest/src/main/kotlin/io/github/xstefanox/demo/mf/rest/handler/RestHandler.kt
@@ -4,6 +4,8 @@ import io.undertow.server.HttpHandler
 import io.undertow.server.HttpServerExchange
 import io.undertow.server.RoutingHandler
 import io.undertow.util.Methods.GET
+import io.undertow.util.Methods.POST
+
 
 class RestHandler : HttpHandler {
 
@@ -11,7 +13,14 @@ class RestHandler : HttpHandler {
     private val helloWorldHandler = HelloWorldHandler()
 
     init {
+
         routingHandler.add(GET, "/hello-world", helloWorldHandler::get)
+
+        routingHandler.add(POST, "/echo", { exchange ->
+            exchange.requestReceiver.receiveFullString { _, message: String? ->
+                exchange.responseSender.send("you sent: $message")
+            }
+        })
     }
 
     override fun handleRequest(exchange: HttpServerExchange?) = routingHandler.handleRequest(exchange)
