af188bbb7a5254288e1dd3630a3f0abd3c70104f
Stefano Varesi <stefano.varesi@gmail.com>
2019-02-01 10:55:19 +0100

    rest config file renamed to rest.conf

--- subproject/rest/src/main/kotlin/io/github/xstefanox/demo/mf/rest/Kodein.kt
+++ subproject/rest/src/main/kotlin/io/github/xstefanox/demo/mf/rest/Kodein.kt
@@ -2,12 +2,20 @@
 
 package io.github.xstefanox.demo.mf.rest
 
+import com.typesafe.config.ConfigFactory
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
 import org.kodein.di.generic.singleton
 
 val REST_MODULE = Kodein.Module("REST") {
-    bind<RestConfiguration>() with singleton { RestConfiguration(instance()) }
+
+    bind<RestConfiguration>() with singleton {
+        val path = "rest"
+        val config = ConfigFactory.load(path)
+        config.checkValid(ConfigFactory.defaultReference(), path)
+        RestConfiguration(config.getConfig(path))
+    }
+
     bind<RestService>() with singleton { RestService(instance()) }
 }
--- subproject/rest/src/main/kotlin/io/github/xstefanox/demo/mf/rest/RestConfiguration.kt
+++ subproject/rest/src/main/kotlin/io/github/xstefanox/demo/mf/rest/RestConfiguration.kt
@@ -1,16 +1,8 @@
 package io.github.xstefanox.demo.mf.rest
 
 import com.typesafe.config.Config
-import com.typesafe.config.ConfigFactory
 
 data class RestConfiguration(private val config: Config) {
-
-    val port: Int
-    val host: String
-
-    init {
-        config.checkValid(ConfigFactory.defaultReference(), "rest")
-        port = config.getInt("rest.port")
-        host = config.getString("rest.host")
-    }
+    val port: Int = config.getInt("port")
+    val host: String = config.getString("host")
 }
