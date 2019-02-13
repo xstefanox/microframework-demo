4cc3c96af858b3da311bf40e6fa4e3efe3bb2cba
Stefano Varesi <stefano.varesi@gmail.com>
2019-02-13 12:47:42 +0100

    rest subproject removed

--- subproject/rest/src/main/kotlin/io/github/xstefanox/demo/mf/rest/RestConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package io.github.xstefanox.demo.mf.rest
-
-import com.typesafe.config.Config
-
-data class RestConfiguration(private val config: Config) {
-    val port: Int = config.getInt("port")
-    val host: String = config.getString("host")
-}
